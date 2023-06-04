ef159010011e47fe4afed514befb0e7f8f2ca1d7
Travis Staloch <twostepted@gmail.com>
2023-06-03 17:50:49 -0700

    main: add mode cli arg
    usage: $ peggen <mode> <file.peg> <?outfile.zig>
      <mode>: gen, print, parse

--- src/main.zig
+++ src/main.zig
@@ -41,8 +41,10 @@ fn chopArg(args: *[]const []const u8) ?[]const u8 {
     return args.*[0];
 }
 
-fn usage(comptime fmt: []const u8) noreturn {
-    std.log.debug(fmt, .{});
+fn usage(comptime fmt: []const u8, args: anytype) noreturn {
+    std.debug.print(fmt ++ "\n", args);
+    std.debug.print("usage: $ peggen <mode> <file.peg> <?outfile.zig> \n", .{});
+    std.debug.print("  <mode>: {s}\n", .{std.meta.fieldNames(Mode)});
     std.os.exit(1);
 }
 
@@ -50,39 +52,51 @@ pub const std_options = struct {
     pub const log_level = .err;
 };
 
+const Mode = enum { gen, print, parse };
+
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     const alloc = arena.allocator();
     var args = try std.process.argsAlloc(alloc);
-    const exec = chopArg(&args).?;
-    _ = exec;
-    const pegfilename = if (chopArg(&args)) |arg|
-        arg
-    else
-        usage("missing first argument: peg file path");
+    _ = chopArg(&args).?;
+    const mode_raw = chopArg(&args) orelse
+        usage("missing argument: <mode>", .{});
+    const mode = std.meta.stringToEnum(Mode, mode_raw) orelse
+        usage("invalid mode: '{s}'", .{mode_raw});
+    const pegfilename = chopArg(&args) orelse
+        usage("missing argument: <file.peg>", .{});
 
     const pegfile = try std.fs.cwd().openFile(pegfilename, .{});
     defer pegfile.close();
     const input = try pegfile.readToEndAlloc(alloc, std.math.maxInt(u32));
     var output = try alloc.alloc(u8, input.len);
 
-    const use_generated_parser = false;
-    if (!use_generated_parser) {
-        var pegparser = PegParser.init(alloc, input, output, pegfilename);
-        const g = try pegparser.parseGrammar();
-        for (g.rules.items) |rule|
-            std.debug.print("{s} <- {}\n", .{ rule.identifier, rule.expression });
-    } else {
-        const generated_parser = @import("../out.zig");
-        const Pg = @import("ParserGenerator.zig");
-        const p = generated_parser.Parser(Pg);
-        var stream = Stream.init(input, output);
-        var ctx = Pg.Context{ .file_path = pegfilename };
-        ctx.flags.insert(.print_errors);
-        const g = try Pg.parse(p.Grammar, &stream, &ctx);
-        std.debug.print("g={s}\n", .{g});
-        // const out = try std.fs.cwd().createFile("out.zig", .{});
-        // defer out.close();
-        // try out.writeAll(try generateFormatted(alloc, g));
+    switch (mode) {
+        .print => {
+            var pegparser = PegParser.init(alloc, input, output, pegfilename);
+            const g = try pegparser.parseGrammar();
+            for (g.rules.items) |rule|
+                std.debug.print("{s} <- {}\n", .{ rule.identifier, rule.expression });
+        },
+        .parse => {
+            const generated_parser = @import("../out.zig");
+            const Pg = @import("ParserGenerator.zig");
+            const p = generated_parser.Parser(Pg);
+            var stream = Stream.init(input, output);
+            var ctx = Pg.Context{ .file_path = pegfilename };
+            ctx.flags.insert(.print_errors);
+            const g = try Pg.parse(p.Grammar, &stream, &ctx);
+            std.debug.print("g={s}\n", .{g});
+        },
+        .gen => {
+            const outfilename = chopArg(&args) orelse
+                usage("missing argument: <?outfile.zig>", .{});
+            var pegparser = PegParser.init(alloc, input, output, pegfilename);
+            const g = try pegparser.parseGrammar();
+            const outfile = try std.fs.cwd().createFile(outfilename, .{});
+            defer outfile.close();
+            const generated = try generateFormatted(alloc, g);
+            try outfile.writeAll(generated);
+        },
     }
 }
